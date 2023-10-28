a3f492bc4a2b86bfefc3a4009c3e5fe9be8497df
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-10-28 12:31:00 -0300

    Create AccesoADatos.cs

---
+++ J3AMS/J3AMS.Negocio/AccesoADatos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3AMS.Negocio
+{ 
+    public class AccesoADatos
+    {
+        private SqlConnection _conexion;
+        private SqlCommand _comando;
+        private SqlDataReader _lector;
+        public SqlDataReader Lector { get { return _lector; } }
+
+        public AccesoADatos()
+        {
+            _conexion = new SqlConnection("server=.\\SQLEXPRESS; database=NAMEDB; integrated security=true");
+            _comando = new SqlCommand();
+        }
+        public void SetConsulta(string consulta)
+        {
+            _comando.CommandType = System.Data.CommandType.Text;
+            _comando.CommandText = consulta;
+        }
+        public void SetProcedure(string sp)
+        {
+            _comando.CommandType = System.Data.CommandType.StoredProcedure;
+            _comando.CommandText = sp;
+        }
+        public void EjecutarLectura()
+        {
+            _comando.Connection = _conexion;
+
+            try
+            {
+                _conexion.Open();
+                _lector = _comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void SetParametro(string parametro, object valor)
+        {
+            _comando.Parameters.AddWithValue(parametro, valor);
+        }
+        public void CerrarConexion()
+        {
+            if (_lector != null)
+                _lector.Close();
+            _conexion.Close();
+        }
+    }
+}
